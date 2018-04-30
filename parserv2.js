seriousme/fastify-swaggergen

commit c250eed15e65e3d33feb149e32036f061cf88275

Handle dereferencing of $ref in the parser

parserv2.js
@@ -3,7 +3,22 @@
 class parserV2 {
   constructor() {
     this.config = { generic: {}, routes: [] };
-    this.schemaID = "http://example.com/schemas/defs";
+    this.schemaStore = {};
+  }
+
+  resolveRef(ref) {
+    const [id, path] = ref.split("#");
+    const schema = this.schemaStore[id];
+    if (schema === undefined) {
+      throw new Error(`Schema with id '${id}' not found`);
+    }
+    const components = path.split("/").slice(1);
+    const reducer = (accumulator, currentValue) => accumulator[currentValue];
+    const result = components.reduce(reducer, schema);
+    if (result === undefined) {
+      throw new Error(`Can't deference schema $ref '${ref}'`);
+    }
+    return result;
   }
 
   fixRefs(schema) {
@@ -11,11 +26,9 @@ class parserV2 {
       const thisItem = schema[item];
       if (typeof thisItem === "object" && thisItem !== null) {
         if (thisItem.$ref) {
-          if (thisItem.$ref.match(/^#/)) {
-            thisItem.$ref = this.schemaID + thisItem.$ref;
-          }
+          schema[item] = this.resolveRef(thisItem.$ref);
         }
-        this.fixRefs(thisItem);
+        this.fixRefs(schema[item]);
       }
     }
   }
@@ -145,20 +158,21 @@ class parserV2 {
     }
   }
 
-  processDefs(definitions) {
-    this.config.schema = {
-      schemaid: this.schemaID,
-      definitions
-    };
+  addSchema(schema, label) {
+    const id = label || schema["$id"] || "";
+    if (this.schemaStore[id] !== undefined) {
+      throw new Error(`Schema with id '${id}' already declared!`);
+    }
+    this.schemaStore[id] = schema;
   }
 
   parse(swagger, schemaID) {
-    this.schemaID = schemaID;
+    if (swagger.definitions !== undefined) {
+      this.addSchema({ definitions: swagger.definitions });
+    }
     for (let item in swagger) {
       if (item === "paths") {
         this.processPaths(swagger.basePath, swagger.paths);
-      } else if (item === "definitions") {
-        this.processDefs(swagger.definitions);
       } else {
         this.config.generic[item] = swagger[item];
         if (item === "basePath") {

index.js
@@ -6,8 +6,7 @@ const fastify = require("fastify")();
 const service = require("./service.js")();
 const swagger = require("./examples/petstore-swagger.v2.json");
 const parser = require("./parserV2")();
-const schemaID = "http://example.com/schemas/defs1";
-const config = parser.parse(swagger, schemaID);
+const config = parser.parse(swagger);
 const routeConf = {};
 
 // AJV misses some validators for int32, int64 etc which ajv-oai adds
@@ -33,8 +32,6 @@ function addRoutes(fastify, opts, next) {
   });
   next();
 }
-
-ajv.addSchema(config.schema, schemaID);
 
 fastify.setSchemaCompiler(function(schema) {
   return ajv.compile(schema);

commit b635e4a99854e729173cb80286c9e704f93a5ade

fixed regression on definitions
https://github.com/seriousme/fastify-swagger-codegen/commit/c250eed15e65e3d33feb149e32036f061cf88275
caused "definitions" to end up in "generic". This commit fixes that.

parserv2.js
@@ -21,14 +21,14 @@ class parserV2 {
     return result;
   }
 
-  fixRefs(schema) {
+  resolveRefs(schema) {
     for (let item in schema) {
       const thisItem = schema[item];
       if (typeof thisItem === "object" && thisItem !== null) {
         if (thisItem.$ref) {
           schema[item] = this.resolveRef(thisItem.$ref);
         }
-        this.fixRefs(schema[item]);
+        this.resolveRefs(schema[item]);
       }
     }
   }
@@ -145,8 +145,6 @@ class parserV2 {
       schema: this.makeSchema(data),
       operationId: data.operationId || this.makeOperationId(operation, path)
     };
-    // schema refs need to be fixed from local schema to remote
-    this.fixRefs(route);
     this.config.routes.push(route);
   }
 
@@ -167,19 +165,22 @@ class parserV2 {
   }
 
   parse(swagger, schemaID) {
-    if (swagger.definitions !== undefined) {
-      this.addSchema({ definitions: swagger.definitions });
-    }
     for (let item in swagger) {
-      if (item === "paths") {
-        this.processPaths(swagger.basePath, swagger.paths);
-      } else {
-        this.config.generic[item] = swagger[item];
-        if (item === "basePath") {
-          this.config.prefix = swagger[item];
-        }
+      switch (item) {
+        case "paths":
+          this.processPaths(swagger.basePath, swagger.paths);
+          break;
+        case "definitions":
+          this.addSchema({ definitions: swagger.definitions });
+        default:
+          this.config.generic[item] = swagger[item];
+          if (item === "basePath") {
+            this.config.prefix = swagger[item];
+          }
       }
     }
+    // schema refs need to be resolved
+    this.resolveRefs(this.config.routes);
     return this.config;
   }
 }